4ec6394ccfea9e67f65fab984872e4efc9cb8a63
thang <thang.buivn@gmail.com>
2017-07-29 23:48:10 +1000

    lincos run

--- exps/gpssm/compare_prediction_propagation/data/gen_data_lincos.py
+++ exps/gpssm/compare_prediction_propagation/data/gen_data_lincos.py
@@ -24,11 +24,11 @@ def lincos(T, process_noise, obs_noise, xprev=None):
 def gen_lincos(no_splits, T_train, T_test):
     for i in range(no_splits):
         np.random.seed(i)
-        # generate a dataset from the kink function above
+        # generate a dataset from the lincos function above
         T = T_train + T_test
         process_noise = 0.2
         obs_noise = 0.05
-        (xtrue, x, y) = kink(T, process_noise, obs_noise)
+        (xtrue, x, y) = lincos(T, process_noise, obs_noise)
         y_train = y[:T_train]
         y_test = y[T_train:]
         np.savetxt('lincos_train_%d.txt'%i, y_train, fmt='%.4f', delimiter=' ')
